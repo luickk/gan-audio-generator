53b18621afedd43eaf04340ebcb64a02a867654f
Luick Klippel <luick.klippel@gmail.com>
2018-05-23 18:22:01 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+python -m pip install keras
+python -m pip install librosa
+python -m pip install uuid
+python -m pip install OptionParser
+python -m pip install librosa
+python -m pip install matplotlib
+python -m pip install numpy
+python -m pip install tensorflow
+python -m pip install tqdm
+python -m pip install matplotlib
+python -m pip install sklearn
+python -m pip install scipy
+python main.py -m train
